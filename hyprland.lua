81e54e08990e316a2cafe8c843411bb0c1d8b27e
svaldez <96221462+sv866729@users.noreply.github.com>
2026-07-22 15:24:05 -0500

    Adding gaming monitor normal monitor worked had ot uninstall everything and reinstall accidently deleted all .config files yayaya

--- hyprland.lua
+++ hyprland.lua
@@ -29,7 +29,13 @@ hl.monitor({
   position = "0x0",
   scale = 1 })
 
-
+hl.monitor({
+  output = "HDMI-A-1",
+  mode = "3440x1440@240",
+  desc =" LG Electronics LG ULTRAGEAR+ 307NTFAED395"
+  position = "auto",
+  scale = 1,
+})
 
 ---------------------
 ---- MY PROGRAMS ----
