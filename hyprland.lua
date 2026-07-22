046bf5b3fa4f9b23e3cd5d561a5294ebb9495fb8
svaldez <96221462+sv866729@users.noreply.github.com>
2026-07-22 15:01:04 -0500

    The comma of DOOM

--- hyprland.lua
+++ hyprland.lua
@@ -30,9 +30,8 @@ hl.monitor({
     scale    = "1",
 })
 hl.monitor({
-  output = "",
   mode = "3440x1440@240",
-  desc =" LG Electronics LG ULTRAGEAR+ 307NTFAED395"
+  desc =" LG Electronics LG ULTRAGEAR+ 307NTFAED395",
   position = "auto",
   scale = 1,
 })
