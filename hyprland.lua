ba37c082d1859efd3fd994685a7860ace5d4c86c
svaldez <96221462+sv866729@users.noreply.github.com>
2026-07-23 09:52:38 -0500

    Testing Wall Paper

--- hyprland.lua
+++ hyprland.lua
@@ -70,11 +70,12 @@ local menu        = "hyprlauncher"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+ hl.on("hyprland.start", function ()
+--  hl.exec_cmd(terminal)
+--  hl.exec_cmd("nm-applet")
+    hl.exec_cmd("hyprpaper")
+--  hl.exec_cmd("waybar & hyprpaper")
+ end)
 
 
 -------------------------------
